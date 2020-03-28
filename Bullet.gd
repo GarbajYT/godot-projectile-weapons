a411e71fd7b4a43bf7175ab12b76ab64efefa1c0
Garbaj <62218238+GarbajYT@users.noreply.github.com>
2020-03-28 15:59:58 -0400

    Update Bullet.gd

--- Bullet.gd
+++ Bullet.gd
@@ -10,7 +10,7 @@ func _ready():
 	
 func _physics_process(delta):
 	if shoot:	
-		apply_impulse(transform.basis.z, -transform.basis.z) 
+		apply_impulse(transform.basis.z, -transform.basis.z * SPEED) 
 
 
 func _on_Area_body_entered(body):
